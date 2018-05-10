91020ef3a2d8e0c986fa939a94d3cc6c3022c964
Veronika Shportko <Veronika_Shportko@epam.com>
2018-05-10 18:48:02 +0300

    testinitialize added

---
+++ GmailTest/GmailTest.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Autofac;
+using lab3Selenium.BusinessObject;
+using lab3Selenium.Configuration;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+
+namespace lab3Selenium
+{
+    [TestClass]
+
+    public class GmailTest
+    {
+        private IWebDriver Driver { get; set; } = AutofacConfiguration.GetContainer().Resolve<IWebDriver>();
+
+        private LoginPageBO LoginPage { get; set; }
+
+        private PasswordPageBO PasswordPage { get; set; }
+
+        [TestInitialize]
+        public void SetupTest()
+        {
+            LoginPage = new LoginPageBO(Driver);
+            PasswordPage = new PasswordPageBO(Driver);
+
+            LoginPage.Navigate(Configuration.TestConfiguration.Url);
+            LoginPage.EnterEmail(Configuration.TestConfiguration.Email);
+            LoginPage.ClickNextButton();
+            PasswordPage.EnterPassword(Configuration.TestConfiguration.Password);
+            PasswordPage.ClickNextButton();
+        }
+
+        [TestCleanup]
+        public void CleanUp()
+        {
+            Driver.Quit();
+            Driver.Dispose();
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+        }
+    }
+}
---
+++ GmailTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GmailTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("EPAM Systems")]
+[assembly: AssemblyProduct("GmailTest")]
+[assembly: AssemblyCopyright("Copyright © EPAM Systems 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3eb8bab8-5479-4524-afee-b8149540cb1a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
